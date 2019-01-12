3897426fbd3c280bc522f867eabf0752db7548ec
chabare <chabare95@gmail.com>
2019-01-12 18:28:42 +0100

    Update recipe models

--- cockout/recipe/models.py
+++ cockout/recipe/models.py
@@ -1,6 +1,17 @@
-class Recipe:
-    pass
+from typing import Set
 
 
 class Ingredient:
-    pass
+    def __init__(self, id: int, rank: int, share: int):
+        self.share = share
+        self.rank = rank
+        self.id = id
+
+    def __hash__(self):
+        return hash(self.id * self.rank * self.share)
+
+
+class Recipe:
+    def __init__(self, id: int, ingredients: Set[Ingredient]):
+        self.id = id
+        self.ingredients = ingredients
